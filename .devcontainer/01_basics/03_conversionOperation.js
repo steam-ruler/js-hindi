30a36de81cafd509efa8c53eb684948de6e41c47
Web Ghost <arctonystark8@gmail.com>
2024-08-04 17:11:29 +0000

    conversion of datatypes

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -24,7 +24,58 @@ let booleanIsLoggedIn = Boolean(isLoggedIn);
 let someNumber = 85;
 
 let stringNumber = String(someNumber);
-console.log(stringNumber); // output => 85
-console.log(typeof (stringNumber)); // output => string
+// console.log(stringNumber); // output => 85
+// console.log(typeof (stringNumber)); // output => string
+
+
+// _______________________________________________ OPERATIONS ___________________________________________________________
+
+let value = 3;
+let negValue = -value
+console.log(negValue); // output => -3
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 3);
+// console.log(2 / 3);
+// console.log(2 % 3);
+
+let str1 = "hello";
+let str2 = "john";
+
+let str3 = str1 + str2;
+// console.log(str3);
+
+// NOT RECOMMENDED
+// console.log("1" + 2); // output => 12
+// console.log(1 + "2"); // output => 12
+// console.log("1" + 2 + 2); // output => 122
+// console.log(1 + 2 + "2"); // output => 32
+
+// console.log(+true); // output => 1
+// console.log(+""); // output => 0
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 2; 
+
+let gameCounter = 100;
+// gameCounter++;
+++gameCounter;
+
+console.log(gameCounter); // output => 101
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
