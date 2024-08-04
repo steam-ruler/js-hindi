e6be705fd2e6c3f5256c000c49bf1d8e72f2a558
Web Ghost <arctonystark8@gmail.com>
2024-08-04 16:26:37 +0000

    Conversion of datatypes

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = "33abc";
+
+// console.log(typeof (score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); 
+
+// "33" => 33
+// "33abc" => Nan | typeof is Number
+// true => 1; false => 0
+// null => 0
+// string => Nan
+
+
+// let isLoggedIn = 1;  // output => true
+// let isLoggedIn = ""; // output => false
+let isLoggedIn = "John"; // output => true
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn); // output => true
+
+
+let someNumber = 85;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber); // output => 85
+console.log(typeof (stringNumber)); // output => string
+
+
